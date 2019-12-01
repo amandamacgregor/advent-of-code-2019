97b7c04cbc309b08317e3fa3fca6c20f5d94e72a
Amanda MacGregor <amanda.macgregor13@gmail.com>
2019-12-01 11:22:05 -0800

    day1 part2 complete

--- day1solution.js
+++ day1solution.js
@@ -16,3 +16,21 @@ const neededFuel = Mass
   .map(mass => mass - 2)
   .reduce((a,b) => a + b, 0);
   console.log(neededFuel) //3263320
+
+  //part two
+// A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+// At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+// The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+const eachModFuel = n => Math.floor(n / 3 - 2)
+
+const fuelTotal = n => {
+    const fuel = eachModFuel(n);
+    return fuel > 0 ? fuel + fuelTotal(fuel) : 0;
+  }
+
+  const sumFuel = Mass
+    .map(fuelTotal)
+    .reduce((a,b) => a + b, 0);
+
+  console.log(sumFuel) //4892135
